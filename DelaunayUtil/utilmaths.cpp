fa188bc0ca54a8b5585b6642ed056124196582ea
chiconasa3000 <csucavelando@gmail.com>
2017-09-22 18:36:20 -0500

    Christian Suca Velando - listaCandidatos

---
+++ DelaunayUtil/utilmaths.cpp
@@ -0,0 +1,6 @@
+#include "utilmaths.h"
+
+UtilMaths::UtilMaths()
+{
+
+}
---
+++ DelaunayUtil/utilmaths.h
@@ -0,0 +1,11 @@
+#ifndef UTILMATHS_H
+#define UTILMATHS_H
+
+
+class UtilMaths
+{
+public:
+    UtilMaths();
+};
+
+#endif // UTILMATHS_H
